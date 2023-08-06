1bdea0c87a6ca43dc91d217abc7eda302709bb6b
Z <a@b.c>
2023-08-05 22:36:29 -0600

    Better installer.

--- install.py
+++ install.py
@@ -11,12 +11,13 @@ def create_symlink(source, destination):
     except Exception as e:
         print(f"Error creating symlink: {e}")
 
-def search_file(filename, path="/"):
+def search_files(extension, path="/"):
     try:
-        result = subprocess.check_output(f"find {path} -name {filename}", shell=True).decode()
-        files = result.split("\n")[:-1]  # Last element is empty due to trailing newline
+        result = subprocess.check_output(f"find {path} -name '*.{extension}'", shell=True).decode()
+        files = result.split("\n")[:-1]
         return files
     except Exception as e:
+        print(f"Error searching for files: {e}")
         return []
 
 parser = argparse.ArgumentParser(description='Search for a file and create a symbolic link if it exists.')
@@ -26,17 +27,20 @@ args = parser.parse_args()
 try:
     if platform.system() == "Linux" and args.symdir:
         print("Linux detected, checking if it's possible to create a symlink instead of downloading a new file.")
-        filename = "sd_xl_base_1.0_0.9vae.safetensors"
-        files = search_file(filename, path=args.symdir)
+        files = search_files("safetensors", path=args.symdir)
 
-        if files:  # If files is not empty, i.e., the file exists
-            destination = "models/sd_xl_base_1.0_0.9vae.safetensors"
-            create_symlink(files[0], destination)
+        if files:
+            print("Found!")
+            for file in files:
+                filename = os.path.basename(file)
+                destination = f"models/{filename}"
+                create_symlink(file, destination)
             exit()
         else:
             print("File not found. Proceed with download.")
 except Exception as e:
-    pass
+    print(e)
+    print("Encountered an error. Proceeding to download.")
 
 repo_id = "stabilityai/stable-diffusion-xl-base-1.0"
     
